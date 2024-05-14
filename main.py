72e3389acb7508e4c8e9b30a8a33000ccf714735
Nikunj Dev <devnikunj22@gmail.com>
2024-05-14 21:03:17 +0530

    Update!

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import pandas as pd
 # Sample dataset: Features of different species (e.g., size, color, habitat)
 # Replace this with your actual data
 data = {
-    'Feature1': [5.1, 7.0, 6.3, 1.2],
-    'Feature2': [3.5, 3.2, 3.3, 5.5],
-    'Feature3': [9.1, 4.0, 7.3, 1.4],
-    'Feature4': [0.2, 1.4, 2.5, 0.5],
-    'Species': ['Species A', 'Species B', 'Species C', 'Species D']
+    'Ear Size': [2.4, 0.13, 4.0],
+    'Body Size': [27.0, 2.0, 4.2],
+    'Eyes Size': [0.17, 0.02, 0.03],
+    'Heart Length': [1.5, 0.12, 0.2],
+    'Endangered Animals': ['Blue Whale', 'White Tailed Deer', 'Black Rhinos']
 }
 
 # Convert the dictionary to a pandas DataFrame
 df = pd.DataFrame(data)
 
 # Define the features and the target variable
-features = df[['Feature1', 'Feature2', 'Feature3', 'Feature4']]
-target = df['Species']
+features = df[['Ear Size', 'Body Size', 'Eyes Size', 'Heart Length']]
+target = df['Endangered Animals']
 
 # Initialize the decision tree classifier
 classifier = tree.DecisionTreeClassifier()
@@ -33,5 +33,36 @@ def predict_species(new_features):
 
 # Example usage of the function
 # Replace the list with actual features to get the species prediction
-example_features = [5.1, 3.5, 9.1, 0.2]  # Example feature set
+example_features = [2.4, 27.0, 0.17, 1.5]  # Example feature set
 print('The predicted species is: ',predict_species(example_features))
+
+plants_data = {
+    'Stem Width': [0.6, 0.05, 0.7],
+    'Stem Length': [22.5, 2.2, 15.5],
+    'Root Length': [10.2, 1.5, 2.5],
+    'Leaf Size': [0.08, 0.4, 0.1],
+    'Endangered Plants': ['White Pine Tree', 'Sunflower', 'Ebony']
+}
+
+df = pd.DataFrame(plants_data)
+
+# Define the features and the target variable
+plant_feature = df[['Stem Width', 'Stem Length', 'Root Length', 'Leaf Size']]
+plant_target = df['Endangered Plants']
+
+# Initialize the decision tree classifier
+classifier = tree.DecisionTreeClassifier()
+
+# Train the classifier with the dataset
+classifier.fit(plant_feature, plant_target)
+
+# Function to predict the species
+def predict_plant(new_plant):
+    # The new_features should be a list of features
+    plant_prediction = classifier.predict([new_plant])
+    return plant_prediction[0]
+
+# Example usage of the function
+# Replace the list with actual features to get the species prediction
+example_plants = [0.05, 2.2, 1.5, 0.4]  # Example feature set
+print('The predicted Endangered Plant is: ',predict_plant(example_plants))
